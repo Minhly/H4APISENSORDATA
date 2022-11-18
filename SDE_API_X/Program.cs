d45cf07b5da48ae4080400d14af38681b3b5506c
Minhly <minh.y.ly@hotmail.com>
2022-11-18 08:59:39 +0100

    First Commit

---
+++ SDE_API_X/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDE_API_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
