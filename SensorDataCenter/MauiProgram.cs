642302f3c03bc8eeda6a60f5d03e676598d37c5d
DavidSKoppel <71077820+DavidSKoppel@users.noreply.github.com>
2022-11-22 12:29:34 +0100

    Test push for maps

--- SensorDataCenter/MauiProgram.cs
+++ SensorDataCenter/MauiProgram.cs
@@ -15,7 +15,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .UseMauiMaps();
 
         builder.Services.AddTransient<TemperatureService>();
         builder.Services.AddTransient<TemperatureViewModel>();
--- SensorDataCenter/View/GeolocationPage.xaml.cs
+++ SensorDataCenter/View/GeolocationPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Maps;
 using SensorDataCenter.ViewModel;
 
 namespace SensorDataCenter.View;
@@ -9,4 +10,19 @@ public partial class GeolocationPage : ContentPage
 		InitializeComponent();
         BindingContext = geolocationViewModel;
     }
+
+	protected override void OnNavigatedTo(NavigatedToEventArgs args)
+	{
+		base.OnNavigatedTo(args);
+
+		var place = new Location(55.1, 11.1);
+
+		var mapSpan = MapSpan.FromCenterAndRadius(place, Distance.FromKilometers(3));
+		map.MoveToRegion(mapSpan);
+		/*map.Pins.Add(new Pin
+		{
+			Label = "Welcome to .NET MAUI!",
+			Location = place
+		});*/
+	}
 }
