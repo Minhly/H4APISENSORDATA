4cc00f4b1e726ecfd16acdcfd400583e88c8c68a
Minhly <minh.y.ly@hotmail.com>
2022-11-23 08:30:31 +0100

    FrontPage V2.0

--- SensorDataCenter/AppShell.xaml.cs
+++ SensorDataCenter/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace SensorDataCenter;
+﻿using SensorDataCenter.View;
+
+namespace SensorDataCenter;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+        Routing.RegisterRoute(nameof(GeolocationPage), typeof(GeolocationPage));
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(PostTempPage), typeof(PostTempPage));
+    }
 }
--- SensorDataCenter/ViewModel/FrontPageViewModel.cs
+++ SensorDataCenter/ViewModel/FrontPageViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using SensorDataCenter.Services;
+using SensorDataCenter.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Android.Content.ClipData;
 
 namespace SensorDataCenter.ViewModel
 {
     public class FrontPageViewModel : BaseViewModel
     {
+        public Command GetTemperatureButton { get; }
+        public Command PostTemperatureButton { get; }
+        public Command GeoLocationButton { get; }
+        public FrontPageViewModel()
+        {
+            GetTemperatureButton = new Command(async () => await RedirectToGetTemperaturePage());
+            PostTemperatureButton = new Command(async () => await RedirectToPostTemperaturePage());
+            GeoLocationButton = new Command(async () => await RedirectToGeoLocationPage());
+        }
+        async Task RedirectToGetTemperaturePage()
+        {
+            await Shell.Current.GoToAsync($"{nameof(MainPage)}");
+        }
+
+        async Task RedirectToPostTemperaturePage()
+        {
+            await Shell.Current.GoToAsync($"{nameof(PostTempPage)}");
+        }
+        async Task RedirectToGeoLocationPage()
+        {
+            await Shell.Current.GoToAsync($"{nameof(GeolocationPage)}");
+        }
     }
 }
